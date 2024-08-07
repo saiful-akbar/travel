045ae48d49c64eb1051aa620196adc48bbe706c4
saiful-akbar <saifulakbar.dev@gmail.com>
2024-08-08 00:38:36 +0700

    fix(register): perbaikan form register

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -2,100 +2,146 @@
     <section class="bg-black overflow-hidden">
         <div class="py-15 py-xl-20 d-flex flex-column container level-3 min-vh-100">
             <div class="row align-items-center justify-content-center my-auto">
-                <div class="col-md-10 col-lg-8 col-xl-6">
+                <div class="col-md-10 col-lg-8">
                     <div class="card">
                         <div class="card-header bg-white text-center pb-0">
                             <h5 class="fs-4 mb-1">Register</h5>
                         </div>
 
                         <div class="card-body bg-white">
-                            <form id="formRegister"
+                            <form
+                                id="formRegister"
+                                method="post"
                                 action="{{ route('register.store', ['redirect' => request('redirect', route('main.home'))]) }}"
-                                method="post">
+                            >
                                 @csrf
 
-                                {{-- field nama lengkap --}}
-                                <div class="form-floating mb-3">
-                                    <input type="text" name="nama_lengkap" id="nama_lengkap"
-                                        value="{{ old('nama_lengkap') }}" placeholder="Masukan nama lengkap..."
-                                        class="form-control @error('nama_lengkap') is-invalid @enderror">
+                                {{-- field nama lengkap dan jenis kelamin --}}
+                                <div class="row">
+                                    <div class="col-md-6 col-12 mb-2">
+                                        <label for="nama_lengkap" class="form-label">
+                                            Nama Lengkap <span class="text-danger">*</span>
+                                        </label>
+    
+                                        <input
+                                            type="text"
+                                            name="nama_lengkap"
+                                            id="nama_lengkap"
+                                            value="{{ old('nama_lengkap') }}"
+                                            placeholder="Nama lengkap..."
+                                            @class([
+                                                'form-control',
+                                                'is-invalid' => $errors->has('nama_lengkap')
+                                            ])
+                                        >
+    
+                                        @error('nama_lengkap')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
 
-                                    <label for="nama_lengkap">
-                                        Nama Lengkap <span class="text-danger">*</span>
-                                    </label>
+                                    <div class="col-md-6 col-12 mb-2">
+                                        <label for="jenis_kelamin" class="form-label">
+                                            Jenis Kelamin <span class="text-danger">*</span>
+                                        </label>
 
-                                    @error('nama_lengkap')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
+                                        <select
+                                            name="jenis_kelamin"
+                                            id="jenis_kelamin"
+                                            @class([
+                                                'form-select',
+                                                'is-invalid' => $errors->has('jenis_kelamin')
+                                            ])
+                                        >
+                                            <option value="" selected disabled>Jenis Kelamin...</option>
+                                            <option value="Laki-Laki" @selected(old('jenis_kelamin') == 'Laki-Laki')>Laki-Laki</option>
+                                            <option value="Perempuan" @selected(old('jenis_kelamin') == 'Perempuan')>Perempuan</option>
+                                        </select>
+    
+    
+                                        @error('jenis_kelamin')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
 
-                                {{-- field jenis kelamin --}}
-                                <div class="form-floating mb-3">
-                                    <select name="jenis_kelamin" id="jenis_kelamin"
-                                        class="form-select @error('jenis_kelamin') is-invalid @enderror">
-                                        <option value="" selected disabled>Pilih Jenis Kelamin...</option>
-                                        <option value="Laki-Laki" @selected(old('jenis_kelamin') == 'Laki-Laki')>Laki-Laki</option>
-                                        <option value="Perempuan" @selected(old('jenis_kelamin') == 'Perempuan')>Perempuan</option>
-                                    </select>
+                                {{-- field nomer telepon dan email --}}
+                                <div class="row">
+                                    <div class="col-md-6 col-12 mb-2">
+                                        <label for="telepon" class="form-label">No. Telepon</label>
 
-                                    <label for="jenis_kelamin">
-                                        Jenis Kelamin <span class="text-danger">*</span>
-                                    </label>
+                                        <input
+                                            type="tel"
+                                            name="telepon"
+                                            id="telepon"
+                                            value="{{ old('telepon') }}"
+                                            placeholder="No. telepon..."
+                                            @class([
+                                                'form-control',
+                                                'is-invalid' => $errors->has('telepon')
+                                            ])
+                                        >
+    
+                                        @error('telepon')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
 
-                                    @error('jenis_kelamin')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-
-                                {{-- field nomer telepon --}}
-                                <div class="form-floating mb-3">
-                                    <input type="tel" name="telepon" id="telepon" value="{{ old('telepon') }}"
-                                        placeholder="Masukan no. telp..."
-                                        class="form-control @error('telepon') is-invalid @enderror">
-
-                                    <label for="telepon">Telepon</label>
-
-                                    @error('telepon')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-
-                                {{-- field email --}}
-                                <div class="form-floating mb-3">
-                                    <input type="email" name="email" id="email" value="{{ old('email') }}"
-                                        placeholder="Masukan email..."
-                                        class="form-control @error('email') is-invalid @enderror">
-
-                                    <label for="email">
-                                        Email <span class="text-danger">*</span>
-                                    </label>
-
-                                    @error('email')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
+                                    <div class="col-md-6 col-12 mb-2">
+                                        <label for="email" class="form-label">
+                                            Email <span class="text-danger">*</span>
+                                        </label>
+    
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            value="{{ old('email') }}"
+                                            placeholder="Email..."
+                                            @class([
+                                                'form-control',
+                                                'is-invalid' => $errors->has('email')
+                                            ])
+                                        >
+    
+                                        @error('email')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
 
                                 {{-- field password --}}
-                                <div class="form-floating">
-                                    <input type="password" name="password" id="password"
-                                        placeholder="Masukan password..."
-                                        class="form-control @error('password') is-invalid @enderror">
-
-                                    <label for="password">
-                                        Password <span class="text-danger">*</span>
-                                    </label>
-
-                                    @error('password')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
+                                <div class="row">
+                                    <div class="col-12">
+                                        <label for="password" class="form-label">
+                                            Password <span class="text-danger">*</span>
+                                        </label>
+    
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            placeholder="Password..."
+                                            @class([
+                                                'form-control',
+                                                'is-invalid' => $errors->has('password')
+                                            ])
+                                        >
+    
+                                        @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
 
                                 <div class="d-grid mt-5">
